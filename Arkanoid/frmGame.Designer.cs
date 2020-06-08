c716d3c47baa3745b3db9f06300d06400f9efc8b
maanuhb <00094119@uca.edu.sv>
2020-06-08 05:03:04 -0600

    Add: spaceship movement and game frame

---
+++ Arkanoid/frmGame.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace Arkanoid
+{
+    partial class frmGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources =
+                new System.ComponentModel.ComponentResourceManager(typeof(frmGame));
+            this.picSpaceShip = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize) (this.picSpaceShip)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picSpaceShip
+            // 
+            this.picSpaceShip.BackColor = System.Drawing.Color.Transparent;
+            this.picSpaceShip.BackgroundImage = global::Arkanoid.Properties.Resources.barra2loop;
+            this.picSpaceShip.Location = new System.Drawing.Point(704, 417);
+            this.picSpaceShip.Name = "picSpaceShip";
+            this.picSpaceShip.Size = new System.Drawing.Size(117, 67);
+            this.picSpaceShip.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picSpaceShip.TabIndex = 0;
+            this.picSpaceShip.TabStop = false;
+            // 
+            // frmGame
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackgroundImage = global::Arkanoid.Properties.Resources.GameBackground;
+            this.ClientSize = new System.Drawing.Size(1348, 634);
+            this.Controls.Add(this.picSpaceShip);
+            this.Icon = ((System.Drawing.Icon) (resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "frmGame";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.frmGame_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmGame_KeyDown);
+            ((System.ComponentModel.ISupportInitialize) (this.picSpaceShip)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picSpaceShip;
+    }
+}
---
+++ Arkanoid/frmGame.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arkanoid
+{
+    public partial class frmGame : Form
+    {
+        public frmGame()
+        {
+            InitializeComponent();
+            Height = ClientSize.Height;
+            Width = ClientSize.Width;
+            WindowState = FormWindowState.Normal;
+            KeyDown += new KeyEventHandler(frmGame_KeyDown);
+            int x = (int) (Width * 0.50);
+            int y = (int) (Height * 0.20);
+            picSpaceShip.Location = new Point(x,y);
+        }
+
+        private void frmGame_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Right:
+                    if ( picSpaceShip.Location.X <=Width*0.75- picSpaceShip.Width  )
+                        picSpaceShip.Left += 10; 
+                    break;
+                case Keys.Left:
+                    if ( picSpaceShip.Location.X >= Width*0.40- picSpaceShip.Width)
+                    
+                        picSpaceShip.Left -= 10; 
+                    break;
+                        
+            }
+        }
+
+        private void frmGame_Load(object sender, EventArgs e)
+        {
+            picSpaceShip.Top = (Height - picSpaceShip.Height) - 130;
+        }
+    }
+}
