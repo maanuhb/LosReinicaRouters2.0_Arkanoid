3be732db85478f09db2cdd6aa8925dfebfb00941
EvaristArdon <00361518@uca.edu.sv>
2020-06-10 00:02:30 -0600

    Modify: images

---
+++ Arkanoid/CustomPictureBox.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+namespace Arkanoid
+{
+    public class CustomPictureBox : PictureBox
+    {
+        public int hits {get; set;}
+
+        public CustomPictureBox() : base()
+        {
+        }
+    }
+}
