8907d2b42db211dde76891f6e1ca9784bc523cae
marioR <mariojosue155@gmail.com>
2020-06-16 20:10:14 -0600

    -

--- Arkanoid/frmGame.cs
+++ Arkanoid/frmGame.cs
@@ -231,10 +231,9 @@ namespace Arkanoid
                 for (int i = 0; i < 4; i++)
                 for (int j = 0; j < 6; j++)
                     if (cpb[i, j] != null)
-                    {
+                    { 
                         return false;
-                    }
-
+                    } 
                 return true;
         }
     }
